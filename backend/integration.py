Undid error of increasing dim of EM sol in PE int. Noted that EM mode fields have flipped sign?? Also noted that only term that contributes to PE int is p_tensor[0,0,2,2] - is that correct???

backend/integration.py
@@ -146,12 +146,12 @@ def gain_and_qs(sim_EM_wguide, sim_AC_wguide, q_acoustic,
         print "\n\n Routine photoelastic_int interrupted by keyboard.\n\n"
 
 
-    print "PE dx", np.max(basis_overlap_PE[:,:,0,:])
-    print "PE dy", np.max(basis_overlap_PE[:,:,1,:])
-    print "PE dz", np.max(basis_overlap_PE[:,:,2,:])
-    print "PE x", np.max(basis_overlap_PE[:,:,:,0])
-    print "PE y", np.max(basis_overlap_PE[:,:,:,1])
-    print "PE z", np.max(basis_overlap_PE[:,:,:,2])
+    # print "PE dx", np.max(basis_overlap_PE[:,:,0,:])
+    # print "PE dy", np.max(basis_overlap_PE[:,:,1,:])
+    # print "PE dz", np.max(basis_overlap_PE[:,:,2,:])
+    # print "PE x", np.max(basis_overlap_PE[:,:,:,0])
+    # print "PE y", np.max(basis_overlap_PE[:,:,:,1])
+    # print "PE z", np.max(basis_overlap_PE[:,:,:,2])
 
     # Q_PE[0,0,2] = Q_PE[0,0,2]/0.388837986772
     # Q_PE[0,0,4] = Q_PE[0,0,4]/0.299436272977

examples/simo_1st_dev.py
@@ -31,7 +31,7 @@ num_EM_modes = 20
 num_AC_modes = 20
 
 EM_ival1=0
-EM_ival2=0
+EM_ival2=EM_ival1
 AC_ival='All'
 # AC_ival=2
 
@@ -59,7 +59,7 @@ np.savez('wguide_data', sim_EM_wguide=sim_EM_wguide)
 # npzfile = np.load('wguide_data.npz')
 # sim_EM_wguide = npzfile['sim_EM_wguide'].tolist()
 # print 'k_z of EM wave \n', sim_EM_wguide.Eig_value
-# plotting.plt_mode_fields(sim_EM_wguide, xlim=0.4, ylim=0.4, EM_AC='EM')
+plotting.plt_mode_fields(sim_EM_wguide, xlim=0.4, ylim=0.4, EM_AC='EM')
 
 
 ### Calculate Acoustic Modes