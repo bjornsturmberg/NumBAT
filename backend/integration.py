Modified array_sol so that uz and u transverse are out of phase. Found that z derivatives of conj u had wrong sigh in AC_alpha. Multiplied z comp of EM field by i so that out of phase w transverse.

backend/integration.py
@@ -38,45 +38,44 @@ def gain_and_qs(sim_EM_wguide, sim_AC_wguide, q_acoustic,
 # phi is values of Lagrange polynomials (1-6) at that node.
 # grad is value of gradient of Lagrange polynomials (1-6) at that node.
 
+
     if EM_ival1 == 'All':
         EM_ival1_fortran = -1
-        P1 = sim_EM_wguide.EM_mode_overlap
-        len_EM1 = len(P1)
+        # P1 = sim_EM_wguide.EM_mode_overlap
     else:
         EM_ival1_fortran = EM_ival1+1  # convert back to fortran indexing
-        P1 = [sim_EM_wguide.EM_mode_overlap[EM_ival1]]
-        len_EM1 = 1
+        # P1 = np.zeros(num_modes_EM, dtype=complex)
+        # P1[EM_ival1] = sim_EM_wguide.EM_mode_overlap[EM_ival1]
 
     if EM_ival2 == 'All':
         EM_ival2_fortran = -1
-        P2 = sim_EM_wguide.EM_mode_overlap
-        len_EM2 = len(P2)
+        # P2 = sim_EM_wguide.EM_mode_overlap
     else:
         EM_ival2_fortran = EM_ival2+1  # convert back to fortran indexing
-        P2 = [sim_EM_wguide.EM_mode_overlap[EM_ival2]]
-        len_EM2 = 1
+        # P2 = np.zeros(num_modes_EM, dtype=complex)
+        # P2[EM_ival2] = sim_EM_wguide.EM_mode_overlap[EM_ival2]
 
     if AC_ival == 'All':
         AC_ival_fortran = -1
         # Note: sim_AC_wguide.Omega_AC if the acoustic angular freq in units of Hz
         AC_freq_Omega = sim_AC_wguide.Omega_AC
-        P3 = sim_AC_wguide.AC_mode_overlap
-        len_AC = len(P3)
+        # P3 = sim_AC_wguide.AC_mode_overlap
     else:
         AC_freq_Omega = sim_AC_wguide.Omega_AC[AC_ival]
         AC_ival_fortran = AC_ival+1  # convert back to fortran indexing
-        P3 = [sim_AC_wguide.AC_mode_overlap[AC_ival]]
-        len_AC = 1
+        # P3 = np.zeros(num_modes_AC, dtype=complex)
+        # P3[AC_ival] = sim_AC_wguide.AC_mode_overlap[AC_ival]
 
     Fortran_debug = 0
     ncomps = 3
     nnodes = 6
-    num_EM_modes = len(sim_EM_wguide.Eig_value)
+    num_modes_EM = sim_EM_wguide.num_modes
+    num_modes_AC = sim_AC_wguide.num_modes
     n_msh_el_AC = sim_AC_wguide.n_msh_el
-    trimmed_EM_field = np.zeros((ncomps,nnodes,num_EM_modes,n_msh_el_AC), dtype=complex)
+    trimmed_EM_field = np.zeros((ncomps,nnodes,num_modes_EM,n_msh_el_AC), dtype=complex)
     for el in range(n_msh_el_AC):
         new_el = sim_AC_wguide.el_convert_tbl[el]
-        for ival in range(num_EM_modes):
+        for ival in range(num_modes_EM):
             for n in range(nnodes):
                 for x in range(ncomps):
                     trimmed_EM_field[x,n,ival,el] = sim_EM_wguide.sol1[x,n,ival,new_el]
@@ -111,17 +110,7 @@ def gain_and_qs(sim_EM_wguide, sim_AC_wguide, q_acoustic,
                 sim_AC_wguide.AC_mode_overlap, Fortran_debug)
     except KeyboardInterrupt:
         print "\n\n Routine ac_alpha_int interrupted by keyboard.\n\n"
-
     alpha = np.real(alpha)
-    # print "dx, dx", np.max(basis_overlap_alpha[:,0,0,:])
-    # print "dy, dx", np.max(basis_overlap_alpha[:,1,0,:])
-    # print "dz, dx", np.max(basis_overlap_alpha[:,2,0,:])
-    # print "dx, dy", np.max(basis_overlap_alpha[:,0,1,:])
-    # print "dy, dy", np.max(basis_overlap_alpha[:,1,1,:])
-    # print "dz, dy", np.max(basis_overlap_alpha[:,2,1,:])
-    # print "dx, dz", np.max(basis_overlap_alpha[:,0,2,:])
-    # print "dy, dz", np.max(basis_overlap_alpha[:,1,2,:])
-    # print "dz, dz", np.max(basis_overlap_alpha[:,2,2,:])
 
 
 ### Calc Q_photoelastic Eq. 33
@@ -156,79 +145,21 @@ def gain_and_qs(sim_EM_wguide, sim_AC_wguide, q_acoustic,
     #                 sim_AC_wguide.sol1,q_acoustic,Fortran_debug)
     # print Q_check
 
-    # print np.max(trimmed_EM_field[0, :, 0, :])
-    # print np.max(trimmed_EM_field[1, :, 0, :])
-    # print np.max(trimmed_EM_field[2, :, 0, :])
-    # print np.max(sim_AC_wguide.sol1[0, :, 2, :])
-    # print np.max(sim_AC_wguide.sol1[1, :, 2, :])
-    # print np.max(sim_AC_wguide.sol1[2, :, 2, :])
-    # z_tmpz = np.conj(trimmed_EM_field[0, :, 0, :])*trimmed_EM_field[0, :, 0, :]*np.conj(sim_AC_wguide.sol1[2, :, 2, :])
-    # print np.max(z_tmp1)
-
-    # overlap = 0
-    # nnodes0 = 6
-    # fields_list=[]
-    # for iel in range(n_msh_el_AC):
-    #     for ltest in range(nnodes0):
-    #         l_eq=2
-    #         k_eq=2
-    #         ind_lp = l_eq + 3*(ltest)
-    #         z_tmp1 = basis_overlap_PE[k_eq,ind_lp,iel]
-    #         fields = np.conj(sim_AC_wguide.sol1[l_eq,ltest, 2, iel])
-    #         # fields = 1.0
-    #         fields_list.append(fields)
-    #         overlap += z_tmp1*fields
-
-    # # print z_tmp1
-    # print np.max(basis_overlap_PE[:,:,k_eq,:,:])
-    # print np.max(abs(basis_overlap_PE[:,:,k_eq,:,:]))
-    # print np.max(basis_overlap_PE[:,:,2,:,:])
-    # print 'O', overlap
-    # print np.max(np.abs(basis_overlap_PE))
-    # print np.max(np.abs(fields_list))
-
-    # # # print "PE dy", np.max(basis_overlap_PE[:,:,1,7:12])
-    # # print "PE dx", np.max(basis_overlap_PE[:,:,0,:])
-    # print "PE dy", np.max(basis_overlap_PE[:,:,1,:])
-    # print "PE dz", np.max(basis_overlap_PE[:,:,2,:])
-    # # print "PE x", np.max(basis_overlap_PE[:,:,:,0])
-    # print "PE y", np.max(basis_overlap_PE[:,:,:,1])
-    # print "PE z", np.max(basis_overlap_PE[:,:,:,2])
-    # print "PE dy", np.max(abs(basis_overlap_PE[:,:,1,:]))
-    # print "PE dz", np.max(abs(basis_overlap_PE[:,:,2,:]))
-    # print "PE y", np.max(abs(basis_overlap_PE[:,:,:,1]))
-    # print "PE z", np.max(abs(basis_overlap_PE[:,:,:,2]))
-
-    # print "field ", np.max(field_overlap_PE[:,:,:,:])
-    # print "field ", np.max(np.real(field_overlap_PE[:,:,:,:]))
-    # print "field ", np.max(abs(np.real(field_overlap_PE[:,:,:,:])))
-    # print "field ", np.max(np.imag(field_overlap_PE[:,:,:,:]))
-    # print "field ", np.max(abs(np.imag(field_overlap_PE[:,:,:,:])))
-    # print "field ", np.max(abs(field_overlap_PE[:,:,:,:]))
-    # # print "field x", np.max(field_overlap_PE[:,:,0])
-    # # print "field y", np.max(field_overlap_PE[:,:,1])
-    # # print "field z", np.max(field_overlap_PE[:,:,2])
-    # # print "PE x", np.max(field_overlap_PE[:,:,0])
-    # # print "PE y", np.max(field_overlap_PE[:,:,1])
-    # # print "PE z", np.max(field_overlap_PE[:,:,2])
-
-    # Q_PE[0,0,2] = Q_PE[0,0,2]/2.71513937066
-    # Q_PE[0,0,4] = Q_PE[0,0,4]/0.198162768689
-    # Q_PE[0,0,8] = Q_PE[0,0,8]/3.58118055063
-
     Q_MB = 0.0 # Haven't implemented Moving Boundary integral (but nor did Rakich)
     Q = Q_PE + Q_MB
     # Note: sim_EM_wguide.omega_EM if the optical angular freq in units of Hz
     gain = 2*sim_EM_wguide.omega_EM*AC_freq_Omega*np.real(Q*np.conj(Q))
-    normal_fact = np.zeros((len_EM1, len_EM2, len_AC), dtype=complex)
-    for i in range(len_EM1):
-        for j in range(len_EM2):
-            for k in range(len_AC):
-                normal_fact[i, j, k] = P1[i]*P2[j]*P3[k]
-    # SBS_gain = gain/normal_fact
+    normal_fact = np.zeros((num_modes_EM, num_modes_EM, num_modes_AC), dtype=complex)
+    for i in range(num_modes_EM):
+        P1 = sim_EM_wguide.EM_mode_overlap[i]
+        for j in range(num_modes_EM):
+            P2 = sim_EM_wguide.EM_mode_overlap[j]
+            for k in range(num_modes_AC):
+                P3 = sim_AC_wguide.AC_mode_overlap[k]
+                normal_fact[i, j, k] = P1*P2*P3
     SBS_gain = np.real(gain/normal_fact)
 
-    return SBS_gain, Q_PE, Q_MB, alpha, P1, P3
+    return SBS_gain, Q_PE, Q_MB, alpha
 
 
 

backend/mode_calcs.py
@@ -207,7 +207,7 @@ class Simmo(object):
             # shift = np.real(AC_velocity*self.q_acoustic/(2.*np.pi))
             # print shift
 
-            
+
             # Using acoustic velocity of shear mode pg 215 Auld vol 1.
             # AC_velocity2 = np.real(np.sqrt(self.structure.c_tensor[3,3][el]/self.structure.rho[el]))
             # # shift_freq2 = AC_velocity2*0.5*self.q_acoustic/(2.*np.pi)
@@ -371,10 +371,6 @@ class Simmo(object):
                 nnodes, self.table_nod, self.type_el, self.x_arr,
                 self.structure.nb_typ_el_AC, self.structure.c_tensor_z, 
                 self.q_acoustic, self.Omega_AC, self.sol1, AC_FEM_debug)
-
-            # self.AC_mode_overlap[4] = self.AC_mode_overlap[4]*0.406172825334
-            # self.AC_mode_overlap[8] = self.AC_mode_overlap[8]*0.315371891081
-
 
         except KeyboardInterrupt:
             print "\n\n FEM routine AC_mode_energy_int",\

backend/plotting.py
@@ -52,6 +52,8 @@ def zeros_int_str(zero_int):
 def plt_mode_fields(sim_wguide, n_points=1000, xlim=None, ylim=None,
                   EM_AC='EM', pdf_png='png', add_name=''):
     """ Plot EM mode fields.
+    NOTE: z component of EM field needs comes scaled by 1/(i beta), 
+    which must be reintroduced!
 
         Args:
             sim_wguide : A :Struct: instance that has had calc_modes calculated
@@ -60,6 +62,9 @@ def plt_mode_fields(sim_wguide, n_points=1000, xlim=None, ylim=None,
             n_points  (int): The number of points across unitcell to \
                 interpolate the field onto.
     """
+
+    if EM_AC is not 'EM' and EM_AC is not 'AC':
+        raise ValueError, "EM_AC must be either 'AC' or 'EM'."
 
     plt.clf()
 
@@ -117,7 +122,10 @@ def plt_mode_fields(sim_wguide, n_points=1000, xlim=None, ylim=None,
                 v_y6p[i] = x_arr[i_ex, 1]
                 v_Ex6p[i] = sim_wguide.sol1[0,i_node,ival,i_el]
                 v_Ey6p[i] = sim_wguide.sol1[1,i_node,ival,i_el]
-                v_Ez6p[i] = sim_wguide.sol1[2,i_node,ival,i_el]
+                if EM_AC == 'EM':
+                    v_Ez6p[i] = sim_wguide.sol1[2,i_node,ival,i_el]*1j*sim_wguide.Eig_value[ival]
+                else:
+                    v_Ez6p[i] = sim_wguide.sol1[2,i_node,ival,i_el]
                 i += 1
 
         v_E6p = np.sqrt(np.abs(v_Ex6p)**2 +
@@ -160,10 +168,9 @@ def plt_mode_fields(sim_wguide, n_points=1000, xlim=None, ylim=None,
         v_plots = [m_ReEx,m_ReEy,m_ReEz,m_ImEx,m_ImEy,m_ImEz,m_AbsE]
         if EM_AC=='EM':
             v_labels = ["Re(E_x)","Re(E_y)","Re(E_z)","Im(E_x)","Im(E_y)","Im(E_z)","Abs(E)"]
-        elif EM_AC=='AC':
-            v_labels = ["Re(u_x)","Re(u_y)","Re(u_z)","Im(u_x)","Im(u_y)","Im(u_z)","Abs(u)"]
         else:
-            raise ValueError, "EM_AC must be either 'AC' or 'EM'."
+            v_labels = ["Re(u_x)","Re(u_y)","Re(u_z)","Im(u_x)","Im(u_y)","Im(u_z)","Abs(u)"]
+
 
         # field plots
         plt.clf()
@@ -204,7 +211,7 @@ def plt_mode_fields(sim_wguide, n_points=1000, xlim=None, ylim=None,
                 n_str = r'n$_{eff} = %(re_k)f6 + %(im_k)f6 i$'% \
                     {'re_k' : np.real(n_eff), 'im_k' : np.imag(n_eff)}
             plt.text(10, 0.3, n_str, fontsize=title_font)
-        elif EM_AC=='AC':
+        else:
             if np.imag(sim_wguide.Eig_value[ival]) < 0:
                 k_str = r'$\Omega/2\pi = %(re_k)f6 %(im_k)f6 i$ GHz'% \
                     {'re_k' : np.real(sim_wguide.Eig_value[ival]*1e-9),
@@ -213,8 +220,6 @@ def plt_mode_fields(sim_wguide, n_points=1000, xlim=None, ylim=None,
                 k_str = r'$\Omega/2\pi = %(re_k)f6 + %(im_k)f6 i$ GHz'% \
                     {'re_k' : np.real(sim_wguide.Eig_value[ival]*1e-9),
                     'im_k' : np.imag(sim_wguide.Eig_value[ival]*1e-9)}
-        else:
-            raise ValueError, "EM_AC must be either 'AC' or 'EM'."
         plt.text(10, 0.5, k_str, fontsize=title_font)
 
         if not os.path.exists("fields"):

examples/simo_1st_dev.py
@@ -77,10 +77,8 @@ print 'Res freq of AC wave (GHz) \n', sim_AC_wguide.Eig_value*1e-9
 # plotting.plt_mode_fields(sim_AC_wguide, EM_AC='AC')
 
 
-# sim_AC_wguide.sol1[2,:,:,:] = sim_AC_wguide.sol1[2,:,:,:]*1j
-
 ### Calculate interaction integrals
-SBS_gain, Q_PE, Q_MB, alpha, P1, P3 = integration.gain_and_qs(
+SBS_gain, Q_PE, Q_MB, alpha = integration.gain_and_qs(
     sim_EM_wguide, sim_AC_wguide, q_acoustic,
     EM_ival1=EM_ival1, EM_ival2=EM_ival2, AC_ival=AC_ival)
 # np.savez('wguide_data_AC_gain', SBS_gain=SBS_gain, alpha=alpha)
@@ -92,17 +90,24 @@ SBS_gain, Q_PE, Q_MB, alpha, P1, P3 = integration.gain_and_qs(
 # print "lc2", wguide.lc2
 # print "lc3", wguide.lc3
 
-print 'alpha / CW alpha', alpha[2]/(1./98.70e-6)
-print 'alpha / CW alpha', alpha[4]/(1./27.75e-6)
-print 'alpha / CW alpha', alpha[8]/(1./43.90e-6)
+# print 'alpha / CW alpha', alpha[0]/(1./186.52e-6)
+# print 'alpha / CW alpha', alpha[1]/(1./142.79e-6)
+print 'alpha 2 / CW alpha', alpha[2]/(1./98.70e-6)
+# print 'alpha / CW alpha', alpha[3]/(1./203.98e-6)
+print 'alpha 4 / CW alpha', alpha[4]/(1./27.75e-6)
+# print 'alpha / CW alpha', alpha[5]/(1./66.69e-6)
+# print 'alpha / CW alpha', alpha[6]/(1./53.06e-6)
+# print 'alpha / CW alpha', alpha[7]/(1./33.01e-6)
+print 'alpha 8 / CW alpha', alpha[8]/(1./43.90e-6)
+# print 'alpha / CW alpha', alpha[9]/(1./74.10e-6)
 
-print 'SBS_gain / CW gain', SBS_gain[0,0,2]/alpha[2]/310.25
-print 'SBS_gain / CW gain', SBS_gain[0,0,4]/alpha[4]/2464.98
-print 'SBS_gain / CW gain', SBS_gain[0,0,8]/alpha[8]/36.55
+print 'SBS_gain 2 / CW gain', SBS_gain[0,0,2]/alpha[2]/310.25
+print 'SBS_gain 4 / CW gain', SBS_gain[0,0,4]/alpha[4]/2464.98
+print 'SBS_gain 8 / CW gain', SBS_gain[0,0,8]/alpha[8]/36.55
 
-print 'SBS_gain / CW gain (using CW alpha)', SBS_gain[0,0,2]/(1./98.70e-6)/310.25
-print 'SBS_gain / CW gain (using CW alpha)', SBS_gain[0,0,4]/(1./27.75e-6)/2464.98
-print 'SBS_gain / CW gain (using CW alpha)', SBS_gain[0,0,8]/(1./43.90e-6)/36.55
+print 'SBS_gain 2 / CW gain (using CW alpha)', SBS_gain[0,0,2]/(1./98.70e-6)/310.25
+print 'SBS_gain 4 / CW gain (using CW alpha)', SBS_gain[0,0,4]/(1./27.75e-6)/2464.98
+print 'SBS_gain 8 / CW gain (using CW alpha)', SBS_gain[0,0,8]/(1./43.90e-6)/36.55
 
 # print 'Q_PE error', np.sqrt(SBS_gain[0,0,2]/(1./98.70e-6)/310.25)
 # print 'Q_PE error', np.sqrt(SBS_gain[0,0,4]/(1./27.75e-6)/2464.98)