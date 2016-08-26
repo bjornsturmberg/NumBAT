Corrected symmetry calculation by removing longitudinal component. Added quiver plots of transverse components.

backend/integration.py
@@ -346,7 +346,7 @@ def gain_and_qs(sim_EM_wguide, sim_AC_wguide, q_acoustic,
 
 
 #### Categorise modes by their symmetries #############################################
-def symmetries(sim_wguide, n_points=100):
+def symmetries(sim_wguide, n_points=10):
     """ Plot EM mode fields.
 
         Args:
@@ -387,15 +387,13 @@ def symmetries(sim_wguide, n_points=100):
 
     sym_list = []
 
-    # for ival in [0]:
-    # for ival in [3]:
     for ival in range(len(sim_wguide.Eig_value)):
         # dense triangulation with multiple points
         v_x6p = np.zeros(6*sim_wguide.n_msh_el)
         v_y6p = np.zeros(6*sim_wguide.n_msh_el)
         v_Ex6p = np.zeros(6*sim_wguide.n_msh_el, dtype=np.complex128)
         v_Ey6p = np.zeros(6*sim_wguide.n_msh_el, dtype=np.complex128)
-        v_Ez6p = np.zeros(6*sim_wguide.n_msh_el, dtype=np.complex128)
+        # v_Ez6p = np.zeros(6*sim_wguide.n_msh_el, dtype=np.complex128)
         v_triang6p = []
 
         i = 0
@@ -417,7 +415,7 @@ def symmetries(sim_wguide, n_points=100):
                 v_y6p[i] = x_arr[i_ex, 1]
                 v_Ex6p[i] = mode_fields[0,i_node,ival,i_el]
                 v_Ey6p[i] = mode_fields[1,i_node,ival,i_el]
-                v_Ez6p[i] = mode_fields[2,i_node,ival,i_el]
+                # v_Ez6p[i] = mode_fields[2,i_node,ival,i_el]
                 i += 1
 
         # dense triangulation with unique points
@@ -440,20 +438,20 @@ def symmetries(sim_wguide, n_points=100):
         ImEx = matplotlib.tri.LinearTriInterpolator(triang6p,v_Ex6p.imag,trifinder=finder)
         ReEy = matplotlib.tri.LinearTriInterpolator(triang6p,v_Ey6p.real,trifinder=finder)
         ImEy = matplotlib.tri.LinearTriInterpolator(triang6p,v_Ey6p.imag,trifinder=finder)
-        ReEz = matplotlib.tri.LinearTriInterpolator(triang6p,v_Ez6p.real,trifinder=finder)
-        ImEz = matplotlib.tri.LinearTriInterpolator(triang6p,v_Ez6p.imag,trifinder=finder)
+        # ReEz = matplotlib.tri.LinearTriInterpolator(triang6p,v_Ez6p.real,trifinder=finder)
+        # ImEz = matplotlib.tri.LinearTriInterpolator(triang6p,v_Ez6p.imag,trifinder=finder)
 
         ### plotting
         # interpolated fields
         m_ReEx = ReEx(v_x,v_y).reshape(n_pts_x,n_pts_y)
         m_ReEy = ReEy(v_x,v_y).reshape(n_pts_x,n_pts_y)
-        m_ReEz = ReEz(v_x,v_y).reshape(n_pts_x,n_pts_y)
+        # m_ReEz = ReEz(v_x,v_y).reshape(n_pts_x,n_pts_y)
         m_ImEx = ImEx(v_x,v_y).reshape(n_pts_x,n_pts_y)
         m_ImEy = ImEy(v_x,v_y).reshape(n_pts_x,n_pts_y)
-        m_ImEz = ImEz(v_x,v_y).reshape(n_pts_x,n_pts_y)
+        # m_ImEz = ImEz(v_x,v_y).reshape(n_pts_x,n_pts_y)
         m_Ex = m_ReEx + 1j*m_ImEx
         m_Ey = m_ReEy + 1j*m_ImEy
-        m_Ez = m_ReEz + 1j*m_ImEz
+        # m_Ez = m_ReEz + 1j*m_ImEz
 
         m_Ex_ymirror = np.zeros((n_pts_x,n_pts_y), dtype=np.complex128)
         m_Ex_xmirror = np.zeros((n_pts_x,n_pts_y), dtype=np.complex128)
@@ -461,31 +459,31 @@ def symmetries(sim_wguide, n_points=100):
         m_Ey_ymirror = np.zeros((n_pts_x,n_pts_y), dtype=np.complex128)
         m_Ey_xmirror = np.zeros((n_pts_x,n_pts_y), dtype=np.complex128)
         m_Ey_rotated = np.zeros((n_pts_x,n_pts_y), dtype=np.complex128)
-        m_Ez_ymirror = np.zeros((n_pts_x,n_pts_y), dtype=np.complex128)
-        m_Ez_xmirror = np.zeros((n_pts_x,n_pts_y), dtype=np.complex128)
-        m_Ez_rotated = np.zeros((n_pts_x,n_pts_y), dtype=np.complex128)
+        # m_Ez_ymirror = np.zeros((n_pts_x,n_pts_y), dtype=np.complex128)
+        # m_Ez_xmirror = np.zeros((n_pts_x,n_pts_y), dtype=np.complex128)
+        # m_Ez_rotated = np.zeros((n_pts_x,n_pts_y), dtype=np.complex128)
         Ex_sigma_y = 0
         Ey_sigma_y = 0
-        Ez_sigma_y = 0
+        # Ez_sigma_y = 0
         Ex_sigma_x = 0
         Ey_sigma_x = 0
-        Ez_sigma_x = 0
+        # Ez_sigma_x = 0
         Ex_C_2 = 0
         Ey_C_2 = 0
-        Ez_C_2 = 0
+        # Ez_C_2 = 0
         # max_E = max(np.max(np.abs(m_Ex)), np.max(np.abs(m_Ey)), np.max(np.abs(m_Ez)))
 
         for ix in range(n_pts_x):
             for iy in range(n_pts_y):
                 m_Ex_ymirror[ix,iy] = (m_Ex[ix,n_pts_y-iy-1])
                 m_Ey_ymirror[ix,iy] = -1*(m_Ey[ix,n_pts_y-iy-1])
-                m_Ez_ymirror[ix,iy] = m_Ez[ix,n_pts_y-iy-1]
+                # m_Ez_ymirror[ix,iy] = m_Ez[ix,n_pts_y-iy-1]
                 m_Ex_xmirror[ix,iy] = -1*(m_Ex[n_pts_x-ix-1,iy])
                 m_Ey_xmirror[ix,iy] = (m_Ey[n_pts_x-ix-1,iy])
-                m_Ez_xmirror[ix,iy] = m_Ez[n_pts_x-ix-1,iy]
+                # m_Ez_xmirror[ix,iy] = m_Ez[n_pts_x-ix-1,iy]
                 m_Ex_rotated[ix,iy] = -1*(m_Ex[n_pts_x-ix-1,n_pts_y-iy-1])
                 m_Ey_rotated[ix,iy] = -1*(m_Ey[n_pts_x-ix-1,n_pts_y-iy-1])
-                m_Ez_rotated[ix,iy] = m_Ez[n_pts_x-ix-1,iy]
+                # m_Ez_rotated[ix,iy] = m_Ez[n_pts_x-ix-1,iy]
                 # Ex_sigma_y += np.abs(m_Ex[ix,iy] - m_Ex_ymirror[ix,iy])/max_E
                 # Ey_sigma_y += np.abs(m_Ey[ix,iy] - m_Ey_ymirror[ix,iy])/max_E
                 # Ez_sigma_y += np.abs(m_Ez[ix,iy] - m_Ez_ymirror[ix,iy])/max_E
@@ -498,21 +496,21 @@ def symmetries(sim_wguide, n_points=100):
 
         Ex_sigma_y = np.sum(np.abs(m_Ex - m_Ex_ymirror))
         Ey_sigma_y = np.sum(np.abs(m_Ey - m_Ey_ymirror))
-        Ez_sigma_y = np.sum(np.abs(m_Ez - m_Ez_ymirror))
+        # Ez_sigma_y = np.sum(np.abs(m_Ez - m_Ez_ymirror))
         Ex_sigma_x = np.sum(np.abs(m_Ex - m_Ex_xmirror))
         Ey_sigma_x = np.sum(np.abs(m_Ey - m_Ey_xmirror))
-        Ez_sigma_x = np.sum(np.abs(m_Ez - m_Ez_xmirror))
+        # Ez_sigma_x = np.sum(np.abs(m_Ez - m_Ez_xmirror))
         Ex_C_2 = np.sum(np.abs(m_Ex - m_Ex_rotated))
         Ey_C_2 = np.sum(np.abs(m_Ey - m_Ey_rotated))
-        Ez_C_2 = np.sum(np.abs(m_Ez - m_Ez_rotated))
-        sigma_y = (Ex_sigma_y + Ey_sigma_y + Ez_sigma_y)/(n_pts_x*n_pts_y)
-        sigma_x = (Ex_sigma_x + Ey_sigma_x + Ez_sigma_x)/(n_pts_x*n_pts_y)
-        C_2 = (Ex_C_2 + Ey_C_2 + Ez_C_2)/(n_pts_x*n_pts_y)
-        # print 'C_2', C_2
-        # print 'sigma_y', sigma_y
-        # print 'sigma_x', sigma_x
+        # Ez_C_2 = np.sum(np.abs(m_Ez - m_Ez_rotated))
+        # sigma_y = (Ex_sigma_y + Ey_sigma_y + Ez_sigma_y)/(n_pts_x*n_pts_y)
+        # sigma_x = (Ex_sigma_x + Ey_sigma_x + Ez_sigma_x)/(n_pts_x*n_pts_y)
+        # C_2 = (Ex_C_2 + Ey_C_2 + Ez_C_2)/(n_pts_x*n_pts_y)
+        sigma_y = (Ex_sigma_y + Ey_sigma_y)/(n_pts_x*n_pts_y)
+        sigma_x = (Ex_sigma_x + Ey_sigma_x)/(n_pts_x*n_pts_y)
+        C_2 = (Ex_C_2 + Ey_C_2)/(n_pts_x*n_pts_y)
 
-        if abs(C_2) > 0.1:
+        if abs(C_2) > 0.2:
             C_2_print = -1
         else:
             C_2_print = 1
@@ -524,9 +522,14 @@ def symmetries(sim_wguide, n_points=100):
             sigma_x_print = -1
         else:
             sigma_x_print = 1
+        # print '------'
+        # print ival
         # print 'C_2', C_2_print
-        # print 'sigma_y', sigma_y_print
+        # print 'C_2', C_2
         # print 'sigma_x', sigma_x_print
+        # print 'sigma_x', sigma_x
+        # print 'sigma_y', sigma_y_print
+        # print 'sigma_y', sigma_y
         sym_list.append([C_2_print, sigma_y_print, sigma_x_print])
 
         # v_plots = [np.real(m_Ex_ymirror),np.real(m_Ey_ymirror),np.real(m_Ez_ymirror),

backend/plotting.py
@@ -197,6 +197,38 @@ def plt_mode_fields(sim_wguide, n_points=1000, xlim=None, ylim=None,
             cbar = plt.colorbar(im, cax=cax)
             cbar.ax.tick_params(labelsize=title_font-10)
 
+        q_step = 100
+        v_x_q = v_x.reshape(n_pts_x,n_pts_y)
+        v_y_q = v_y.reshape(n_pts_x,n_pts_y)
+        v_x_q = v_x_q[0::q_step,0::q_step]
+        v_y_q = v_y_q[0::q_step,0::q_step]
+        m_ReEx_q = m_ReEx[0::q_step,0::q_step]
+        m_ReEy_q = m_ReEy[0::q_step,0::q_step]
+        m_ImEx_q = m_ImEx[0::q_step,0::q_step]
+        m_ImEy_q = m_ImEy[0::q_step,0::q_step]
+        ax = plt.subplot(3,3,i_p+2)
+        plt.quiver(v_x_q, v_y_q, (m_ReEx_q+m_ImEx_q), (m_ReEy_q+m_ImEy_q),      # data
+                   np.sqrt(np.real((m_ReEx_q+1j*m_ImEx_q)*(m_ReEx_q-1j*m_ImEx_q)+(m_ReEy_q+1j*m_ImEy_q)*(m_ReEy_q-1j*m_ImEy_q))),  #colour the arrows based on this array
+                   cmap='inferno',     # colour map
+                   pivot='mid',
+                   headlength=5)        # length of the arrows
+        # no ticks
+        ax.set_aspect('equal')
+        plt.xticks([])
+        plt.yticks([])
+        # limits
+        if xlim:
+            ax.set_xlim(xlim*n_points,(1-xlim)*n_points)
+        if ylim:
+            ax.set_ylim(ylim*n_points,(1-ylim)*n_points)
+        # titles
+        plt.title('Transverse',fontsize=title_font)
+        # divider = make_axes_locatable(ax)
+        # cax = divider.append_axes("right", size="5%", pad=0.1)
+        # cbar = plt.colorbar(im, cax=cax)
+
+
+
         if EM_AC=='EM':
             n_eff = sim_wguide.Eig_value[ival] * sim_wguide.wl_m / (2*np.pi)
             if np.imag(sim_wguide.Eig_value[ival]) < 0:
@@ -211,8 +243,9 @@ def plt_mode_fields(sim_wguide, n_points=1000, xlim=None, ylim=None,
                     'im_k' : np.imag(sim_wguide.Eig_value[ival])}
                 n_str = r'n$_{eff} = %(re_k)f6 + %(im_k)f6 i$'% \
                     {'re_k' : np.real(n_eff), 'im_k' : np.imag(n_eff)}
-            plt.text(10, 0.3, n_str, fontsize=title_font)
+            # plt.text(10, 0.3, n_str, fontsize=title_font)
         else:
+            n_str = ''
             if np.imag(sim_wguide.Eig_value[ival]) < 0:
                 k_str = r'$\Omega/2\pi = %(re_k)f6 %(im_k)f6 i$ GHz'% \
                     {'re_k' : np.real(sim_wguide.Eig_value[ival]*1e-9),
@@ -221,7 +254,8 @@ def plt_mode_fields(sim_wguide, n_points=1000, xlim=None, ylim=None,
                 k_str = r'$\Omega/2\pi = %(re_k)f6 + %(im_k)f6 i$ GHz'% \
                     {'re_k' : np.real(sim_wguide.Eig_value[ival]*1e-9),
                     'im_k' : np.imag(sim_wguide.Eig_value[ival]*1e-9)}
-        plt.text(10, 0.5, k_str, fontsize=title_font)
+        # plt.text(10, 0.5, k_str, fontsize=title_font)
+        plt.suptitle(k_str + n_str, fontsize=title_font)
 
         if not os.path.exists("fields"):
             os.mkdir("fields")

examples/simo_1st_dev-As2S3-Silica-Blair.py
@@ -77,13 +77,13 @@ wguide = objects.Struct(unitcell_x,inc_a_x,unitcell_y,inc_a_y,inc_shape,
 
 
 ### Calculate Electromagnetic Modes
-n_eff = 2.5
+n_eff = 2.24
 shift_Hz = n_eff**2 * (2*np.pi/(wl_nm*1e-9))**2
 # sim_EM_wguide = wguide.calc_EM_modes(wl_nm, num_EM_modes, shift_Hz=shift_Hz)
 # np.savez('wguide_data-chalc-Blair', sim_EM_wguide=sim_EM_wguide)
 npzfile = np.load('wguide_data-chalc-Blair.npz')
 sim_EM_wguide = npzfile['sim_EM_wguide'].tolist()
-# print 'k_z of EM wave \n', sim_EM_wguide.Eig_value
+print 'k_z of EM wave \n', sim_EM_wguide.Eig_value
 # plotting.plt_mode_fields(sim_EM_wguide, xlim=0.4, ylim=0.4, EM_AC='EM')
 
 
@@ -91,19 +91,21 @@ sim_EM_wguide = npzfile['sim_EM_wguide'].tolist()
 # Backward SBS
 # Acoustic k has to push optical mode from +ve lightline to -ve, hence factor 2.
 q_acoustic = 2*sim_EM_wguide.Eig_value[0]
+print q_acoustic
+q_acoustic = 2*n_eff*2*np.pi/(wl_nm*1e-9)
 # # Forward (intramode) SBS
 # q_acoustic = 0.0
 # As2S3_bulk_velocity = 2595m/s @1550 gives 7.6 GHz as freq in reg waveguides
 shift_Hz = 5.8e9
 sim_AC_wguide = wguide.calc_AC_modes(wl_nm, q_acoustic, num_AC_modes, 
-   EM_sim=sim_EM_wguide, shift_Hz=shift_Hz )
+   EM_sim=sim_EM_wguide)#, shift_Hz=shift_Hz )
 # np.savez('wguide_data_AC', sim_AC_wguide=sim_AC_wguide)
 # npzfile = np.load('wguide_data_AC.npz')
 # sim_AC_wguide = npzfile['sim_AC_wguide'].tolist()
 print 'Res freq of AC wave (GHz) \n', sim_AC_wguide.Eig_value*1e-9
 # plotting.plt_mode_fields(sim_AC_wguide, EM_AC='AC')
 # sim_AC_wguide.sol1[2,:,:,:] = sim_AC_wguide.sol1[2,:,:,:]*1j
-plotting.plt_mode_fields(sim_AC_wguide, xlim=0.4, ylim=0.4, EM_AC='AC')
+# plotting.plt_mode_fields(sim_AC_wguide, xlim=0.4, ylim=0.4, EM_AC='AC')
 
 
 # # import time
@@ -119,24 +121,24 @@ plotting.plt_mode_fields(sim_AC_wguide, xlim=0.4, ylim=0.4, EM_AC='AC')
 # # SBS_gain = npzfile['SBS_gain']
 # # alpha = npzfile['alpha']
 
-# # # trim1 = 5
-# # # trim = 13
-# # syms = integration.symmetries(sim_AC_wguide)
-# # # for i in range(trim-trim1):
-# # for i in range(num_AC_modes):
-# #    # i = i+trim1
-# #    sym = syms[i]
-# #    print 'Res freq', sim_AC_wguide.Eig_value[i]*1e-9
-# #    print 'SES gain', SBS_gain[0,0,i]/alpha[i]
-# #    print 'alpha', alpha[i]
-# #    if sym[0] == 1 and sym[1] == 1 and sym[2] == 1:
-# #       print 'A'
-# #    if sym[0] == -1 and sym[1] == 1 and sym[2] == -1:
-# #       print 'B1'
-# #    if sym[0] == 1 and sym[1] == -1 and sym[2] == -1:
-# #       print 'B2'
-# #    if sym[0] == -1 and sym[1] == -1 and sym[2] == 1:
-# #       print 'B3'
+# # trim1 = 5
+# # trim = 13
+# syms = integration.symmetries(sim_AC_wguide)
+# # for i in range(trim-trim1):
+# for i in range(num_AC_modes):
+#    # i = i+trim1
+#    sym = syms[i]
+#    print 'Res freq', sim_AC_wguide.Eig_value[i]*1e-9
+#    # print 'SES gain', SBS_gain[0,0,i]/alpha[i]
+#    # print 'alpha', alpha[i]
+#    if sym[0] == 1 and sym[1] == 1 and sym[2] == 1:
+#       print 'A'
+#    if sym[0] == -1 and sym[1] == 1 and sym[2] == -1:
+#       print 'B1'
+#    if sym[0] == 1 and sym[1] == -1 and sym[2] == -1:
+#       print 'B2'
+#    if sym[0] == -1 and sym[1] == -1 and sym[2] == 1:
+#       print 'B3'
 
 # # print "Gain", SBS_gain[0,0,AC_ival]/alpha[AC_ival]
 

examples/simo_1st_dev.py
@@ -69,12 +69,12 @@ q_acoustic = 2*np.real(sim_EM_wguide.Eig_value[0])
 # Forward (intramode) SBS
 # q_acoustic = 0.0
 sim_AC_wguide = wguide.calc_AC_modes(wl_nm, q_acoustic, 
-	num_AC_modes, EM_sim=sim_EM_wguide)#, shift_Hz=20e9)
+	num_AC_modes, EM_sim=sim_EM_wguide)#, shift_Hz=18e9)
 # np.savez('wguide_data_AC', sim_AC_wguide=sim_AC_wguide)
 # npzfile = np.load('wguide_data_AC.npz')
 # sim_AC_wguide = npzfile['sim_AC_wguide'].tolist()
 print 'Res freq of AC wave (GHz) \n', sim_AC_wguide.Eig_value*1e-9
-# plotting.plt_mode_fields(sim_AC_wguide, EM_AC='AC')
+# plotting.plt_mode_fields(sim_AC_wguide, EM_AC='AC')#, add_name='flipped')
 
 
 ### Calculate interaction integrals
@@ -90,25 +90,25 @@ SBS_gain, Q_PE, Q_MB, alpha = integration.gain_and_qs(
 # print "lc2", wguide.lc2
 # print "lc3", wguide.lc3
 
-print 'alpha', alpha[0]
-print 'alpha', alpha[1]
-print 'alpha', alpha[2]
-print 'alpha', alpha[3]
-print 'alpha', alpha[4]
-print 'alpha', alpha[5]
-print 'alpha', alpha[6]
-print 'alpha', alpha[7]
-print 'alpha', alpha[8]
+# print 'alpha', alpha[0]
+# print 'alpha', alpha[1]
+# print 'alpha', alpha[2]
+# print 'alpha', alpha[3]
+# print 'alpha', alpha[4]
+# print 'alpha', alpha[5]
+# print 'alpha', alpha[6]
+# print 'alpha', alpha[7]
+# print 'alpha', alpha[8]
 
-# print 'alpha / CW alpha', alpha[0]/(1./186.52e-6)
-# print 'alpha / CW alpha', alpha[1]/(1./142.79e-6)
-# print 'alpha 2 / CW alpha', alpha[2]/(1./98.70e-6)
-# print 'alpha / CW alpha', alpha[3]/(1./203.98e-6)
-# print 'alpha 4 / CW alpha', alpha[4]/(1./27.75e-6)
-# print 'alpha / CW alpha', alpha[5]/(1./66.69e-6)
-# print 'alpha / CW alpha', alpha[6]/(1./53.06e-6)
-# print 'alpha / CW alpha', alpha[7]/(1./33.01e-6)
-# print 'alpha 8 / CW alpha', alpha[8]/(1./43.90e-6)
+print 'alpha / CW alpha', alpha[0]/(1./186.52e-6)
+print 'alpha / CW alpha', alpha[1]/(1./142.79e-6)
+print 'alpha 2 / CW alpha', alpha[2]/(1./98.70e-6)
+print 'alpha / CW alpha', alpha[3]/(1./203.98e-6)
+print 'alpha 4 / CW alpha', alpha[4]/(1./27.75e-6)
+print 'alpha / CW alpha', alpha[5]/(1./66.69e-6)
+print 'alpha / CW alpha', alpha[6]/(1./53.06e-6)
+print 'alpha / CW alpha', alpha[7]/(1./33.01e-6)
+print 'alpha 8 / CW alpha', alpha[8]/(1./43.90e-6)
 
 # print 'SBS_gain 2 / CW gain', SBS_gain[0,0,2]/alpha[2]/310.25
 # print 'SBS_gain 4 / CW gain', SBS_gain[0,0,4]/alpha[4]/2464.98