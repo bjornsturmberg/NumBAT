edbd3020c609d6488ae5caab1975e66ba2a66b52
Bjorn Sturmberg <bjorn.sturmberg@mq.edu.au>
2016-08-29 10:23:14 +1000

    About to merge in MQ day.

--- examples/simo_1st_dev.py
+++ examples/simo_1st_dev.py
@@ -86,15 +86,13 @@ print 'Res freq of AC wave (GHz) \n', sim_AC_wguide.Eig_value*1e-9
 #         for n in range(6):
 #             local_nod = sim_AC_wguide.table_nod[n][el]-1
 #             # sim_AC_wguide.sol1[0,n,ival,el] = grad*sim_AC_wguide.x_arr[0,local_nod]
-#             sim_AC_wguide.sol1[1,n,ival,el] = grad*sim_AC_wguide.x_arr[0,local_nod]
+#             # sim_AC_wguide.sol1[1,n,ival,el] = grad*sim_AC_wguide.x_arr[0,local_nod]
 #             # sim_AC_wguide.sol1[2,n,ival,el] = grad*sim_AC_wguide.x_arr[0,local_nod]
 #             sim_AC_wguide.sol1[0,n,ival,el] = 0.0
-#             # sim_AC_wguide.sol1[1,n,ival,el] = 0.0
-#             sim_AC_wguide.sol1[2,n,ival,el] = 0.0
+#             sim_AC_wguide.sol1[1,n,ival,el] = 0.0
+#             sim_AC_wguide.sol1[2,n,ival,el] = 1.0
 
 # plotting.plt_mode_fields(sim_AC_wguide, EM_AC='AC')
-
-
 
 
 
@@ -112,9 +110,9 @@ SBS_gain, Q_PE, Q_MB, alpha = integration.gain_and_qs(
 # print "lc2", wguide.lc2
 # print "lc3", wguide.lc3
 
-# print 'alpha', alpha[0]
-# area = inc_a_x*inc_a_y*1e-18
-# print 'alpha', alpha[0]/area
+print 'alpha', alpha[0]
+area = inc_a_x*inc_a_y*1e-18
+print 'alpha', alpha[0]/area
 # print 'alpha', alpha[0]/p_11
 # print 'alpha', alpha[1]
 # print 'alpha', alpha[2]
@@ -135,13 +133,13 @@ print 'alpha / CW alpha', alpha[6]/(1./53.06e-6)
 print 'alpha / CW alpha', alpha[7]/(1./33.01e-6)
 print 'alpha 8 / CW alpha', alpha[8]/(1./43.90e-6)
 
-print 'SBS_gain 2 / CW gain', SBS_gain[0,0,2]/alpha[2]/310.25
-print 'SBS_gain 4 / CW gain', SBS_gain[0,0,4]/alpha[4]/2464.98
-print 'SBS_gain 8 / CW gain', SBS_gain[0,0,8]/alpha[8]/36.55
+# print 'SBS_gain 2 / CW gain', SBS_gain[0,0,2]/alpha[2]/310.25
+# print 'SBS_gain 4 / CW gain', SBS_gain[0,0,4]/alpha[4]/2464.98
+# print 'SBS_gain 8 / CW gain', SBS_gain[0,0,8]/alpha[8]/36.55
 
-print 'SBS_gain 2 / CW gain (using CW alpha)', SBS_gain[0,0,2]/(1./98.70e-6)/310.25
-print 'SBS_gain 4 / CW gain (using CW alpha)', SBS_gain[0,0,4]/(1./27.75e-6)/2464.98
-print 'SBS_gain 8 / CW gain (using CW alpha)', SBS_gain[0,0,8]/(1./43.90e-6)/36.55
+# print 'SBS_gain 2 / CW gain (using CW alpha)', SBS_gain[0,0,2]/(1./98.70e-6)/310.25
+# print 'SBS_gain 4 / CW gain (using CW alpha)', SBS_gain[0,0,4]/(1./27.75e-6)/2464.98
+# print 'SBS_gain 8 / CW gain (using CW alpha)', SBS_gain[0,0,8]/(1./43.90e-6)/36.55
 
 # # print 'Q_PE error', np.sqrt(SBS_gain[0,0,2]/(1./98.70e-6)/310.25)
 # # print 'Q_PE error', np.sqrt(SBS_gain[0,0,4]/(1./27.75e-6)/2464.98)
