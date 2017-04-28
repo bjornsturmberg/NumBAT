Yang, Blair power sesh to fix up fixed Q calculation of alpha and fixed linewidth of Lorenzian plotting.

backend/integration.py
@@ -168,7 +168,9 @@ def gain_and_qs(sim_EM_pump, sim_EM_Stokes, sim_AC, k_AC,
         end = time.time()
         print("     time (sec.)", (end - start))
     else:
-        alpha = (np.pi*k_AC/fixed_Q)*np.ones(num_modes_AC)
+        # alpha = Omega_AC/(vg*fixed_Q) = k_AC/fixed_Q
+        # factor of a half because alpha is for power!
+        alpha = 0.5*(k_AC/fixed_Q)*np.ones(num_modes_AC)
 
 
     # Calc Q_photoelastic Eq. 33

backend/plotting.py
@@ -97,8 +97,8 @@ def gain_specta(sim_AC_wguide, SBS_gain, SBS_gain_PE, SBS_gain_MB, alpha, k_AC,
     # Line width of resonances should be v_g * alpha,
     # but we don't have convenient access to v_g, therefore
     # phase velocity as approximation to group velocity
-    phase_v = sim_AC_wguide.Eig_values/k_AC
-    linewidth = phase_v*alpha
+    phase_v = 2*np.pi*sim_AC_wguide.Eig_values/k_AC
+    linewidth = phase_v*alpha/(2*np.pi)
 
     interp_grid = np.linspace(freq_min, freq_max, num_interp_pts)
     interp_values = np.zeros(num_interp_pts)