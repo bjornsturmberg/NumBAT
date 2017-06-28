b3e4db9b9cd25d6048590f54cef607cdbe9366c4
Bjorn Sturmberg <bjorn.sturmberg@mq.edu.au>
2017-06-29 00:05:49 +1000

    Cleaning of json load script.

--- backend/materials.py
+++ backend/materials.py
@@ -19,12 +19,13 @@ data_location = '../backend/material_data/'
 
 
 class Material(object):
-    """ Represents a material with a refractive index n.
+    """ Represents a material with:
 
-        TODO
-
-        Args:
-            todo
+            Refractive index []
+            Density [kg/m3]
+            Stiffness tensor component [Pa]
+            Photoelastic tensor component []
+            Acoustic loss tensor component [Pa s]
 
         Currently included materials are;
 
@@ -43,144 +44,15 @@ class Material(object):
         +--------------------+
 
     """
-    # def __init__(self, mat_props,
-    #              doi=None, date=None, author=None):
-    #     self.doi = doi
-    #     self.date = date
-    #     self.author = author
-    #     self.n = mat_props[0]
-    #     self.s = mat_props[1]
-    #     if len(mat_props) == 11:
-    #         self.c_11 = mat_props[2]
-    #         self.c_12 = mat_props[3]
-    #         self.c_44 = mat_props[4]
-    #         self.p_11 = mat_props[5]
-    #         self.p_12 = mat_props[6]
-    #         self.p_44 = mat_props[7]
-    #         self.eta_11 = mat_props[8]
-    #         self.eta_12 = mat_props[9]
-    #         self.eta_44 = mat_props[10]
-    #     # Fully anisotropic case
-    #     else:
-    #         self.c_11 = mat_props[2]
-    #         self.c_12 = mat_props[3]
-    #         self.c_13 = mat_props[4]
-    #         self.c_14 = mat_props[5]
-    #         self.c_15 = mat_props[6]
-    #         self.c_16 = mat_props[7]
-    #         self.c_21 = mat_props[8]
-    #         self.c_22 = mat_props[9]
-    #         self.c_23 = mat_props[10]
-    #         self.c_24 = mat_props[11]
-    #         self.c_25 = mat_props[12]
-    #         self.c_26 = mat_props[13]
-    #         self.c_31 = mat_props[14]
-    #         self.c_32 = mat_props[15]
-    #         self.c_33 = mat_props[16]
-    #         self.c_34 = mat_props[17]
-    #         self.c_35 = mat_props[18]
-    #         self.c_36 = mat_props[19]
-    #         self.c_41 = mat_props[20]
-    #         self.c_42 = mat_props[21]
-    #         self.c_43 = mat_props[22]
-    #         self.c_44 = mat_props[23]
-    #         self.c_45 = mat_props[24]
-    #         self.c_46 = mat_props[25]
-    #         self.c_51 = mat_props[26]
-    #         self.c_52 = mat_props[27]
-    #         self.c_53 = mat_props[28]
-    #         self.c_54 = mat_props[29]
-    #         self.c_55 = mat_props[30]
-    #         self.c_56 = mat_props[31]
-    #         self.c_61 = mat_props[32]
-    #         self.c_62 = mat_props[33]
-    #         self.c_63 = mat_props[34]
-    #         self.c_64 = mat_props[35]
-    #         self.c_65 = mat_props[36]
-    #         self.c_66 = mat_props[37]
-
-    #         self.p_11 = mat_props[36+2]
-    #         self.p_12 = mat_props[36+3]
-    #         self.p_13 = mat_props[36+4]
-    #         self.p_14 = mat_props[36+5]
-    #         self.p_15 = mat_props[36+6]
-    #         self.p_16 = mat_props[36+7]
-    #         self.p_21 = mat_props[36+8]
-    #         self.p_22 = mat_props[36+9]
-    #         self.p_23 = mat_props[36+10]
-    #         self.p_24 = mat_props[36+11]
-    #         self.p_25 = mat_props[36+12]
-    #         self.p_26 = mat_props[36+13]
-    #         self.p_31 = mat_props[36+14]
-    #         self.p_32 = mat_props[36+15]
-    #         self.p_33 = mat_props[36+16]
-    #         self.p_34 = mat_props[36+17]
-    #         self.p_35 = mat_props[36+18]
-    #         self.p_36 = mat_props[36+19]
-    #         self.p_41 = mat_props[36+20]
-    #         self.p_42 = mat_props[36+21]
-    #         self.p_43 = mat_props[36+22]
-    #         self.p_44 = mat_props[36+23]
-    #         self.p_45 = mat_props[36+24]
-    #         self.p_46 = mat_props[36+25]
-    #         self.p_51 = mat_props[36+26]
-    #         self.p_52 = mat_props[36+27]
-    #         self.p_53 = mat_props[36+28]
-    #         self.p_54 = mat_props[36+29]
-    #         self.p_55 = mat_props[36+30]
-    #         self.p_56 = mat_props[36+31]
-    #         self.p_61 = mat_props[36+32]
-    #         self.p_62 = mat_props[36+33]
-    #         self.p_63 = mat_props[36+34]
-    #         self.p_64 = mat_props[36+35]
-    #         self.p_65 = mat_props[36+36]
-    #         self.p_66 = mat_props[36+37]
-
-    #         self.eta_11 = mat_props[36+36+2]
-    #         self.eta_12 = mat_props[36+36+3]
-    #         self.eta_13 = mat_props[36+36+4]
-    #         self.eta_14 = mat_props[36+36+5]
-    #         self.eta_15 = mat_props[36+36+6]
-    #         self.eta_16 = mat_props[36+36+7]
-    #         self.eta_21 = mat_props[36+36+8]
-    #         self.eta_22 = mat_props[36+36+9]
-    #         self.eta_23 = mat_props[36+36+10]
-    #         self.eta_24 = mat_props[36+36+11]
-    #         self.eta_25 = mat_props[36+36+12]
-    #         self.eta_26 = mat_props[36+36+13]
-    #         self.eta_31 = mat_props[36+36+14]
-    #         self.eta_32 = mat_props[36+36+15]
-    #         self.eta_33 = mat_props[36+36+16]
-    #         self.eta_34 = mat_props[36+36+17]
-    #         self.eta_35 = mat_props[36+36+18]
-    #         self.eta_36 = mat_props[36+36+19]
-    #         self.eta_41 = mat_props[36+36+20]
-    #         self.eta_42 = mat_props[36+36+21]
-    #         self.eta_43 = mat_props[36+36+22]
-    #         self.eta_44 = mat_props[36+36+23]
-    #         self.eta_45 = mat_props[36+36+24]
-    #         self.eta_46 = mat_props[36+36+25]
-    #         self.eta_51 = mat_props[36+36+26]
-    #         self.eta_52 = mat_props[36+36+27]
-    #         self.eta_53 = mat_props[36+36+28]
-    #         self.eta_54 = mat_props[36+36+29]
-    #         self.eta_55 = mat_props[36+36+30]
-    #         self.eta_56 = mat_props[36+36+31]
-    #         self.eta_61 = mat_props[36+36+32]
-    #         self.eta_62 = mat_props[36+36+33]
-    #         self.eta_63 = mat_props[36+36+34]
-    #         self.eta_64 = mat_props[36+36+35]
-    #         self.eta_65 = mat_props[36+36+36]
-    #         self.eta_66 = mat_props[36+36+37]
-
-
     def __init__(self,data_file):
 
         self.load_data_file(data_file)
 
+
     def load_data_file(self, data_file, alt_path=''):  
         """
-
+        Load data from json file.
+        
         Args:
             data_file  (str): name of data file located in NumBAT/backend/material_data
             alt_path  (str): non standard path to data_file
