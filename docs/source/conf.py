061def9c050b18bdb10b01b9f6848f331fde73bd
Bjorn Sturmberg <bjorn.sturmberg@mq.edu.au>
2017-09-05 23:42:22 +1000

    Nother doc attempt.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,12 +16,12 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
-
-import sys
 import os
+import sys
+sys.path.insert(0, os.path.abspath('.'))
+
+# import sys
+# import os
 
 # class Mock(object):
 #     __all__ = []
