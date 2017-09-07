c889cea7d78e09b0a9e602fe129cca4adb3a2050
Bjorn Sturmberg <bjorn.sturmberg@mq.edu.au>
2017-09-07 21:39:43 +1000

    riten

--- backend/test.py
+++ backend/test.py
@@ -5,9 +5,9 @@ from scipy import sqrt
 import subprocess
 # from matplotlib.mlab import griddata
 # from scipy import interpolate
-import matplotlib
-matplotlib.use('pdf')
-import matplotlib.pyplot as plt
+# import matplotlib
+# matplotlib.use('pdf')
+# import matplotlib.pyplot as plt
 # import matplotlib.gridspec as gridspec
 # from mpl_toolkits.axes_grid1 import make_axes_locatable
 # from matplotlib import ticker
