912210ebefc98d7f8338a77ff457e383d54e92a6
Bjorn Sturmberg <bjorn.sturmberg@mq.edu.au>
2017-09-07 21:18:19 +1000

    test

--- backend/test.py
+++ backend/test.py
@@ -1,16 +1,16 @@
 
-import os
-import numpy as np
-from scipy import sqrt
-import subprocess
-from matplotlib.mlab import griddata
-from scipy import interpolate
-import matplotlib
-matplotlib.use('pdf')
-import matplotlib.pyplot as plt
-import matplotlib.gridspec as gridspec
-from mpl_toolkits.axes_grid1 import make_axes_locatable
-from matplotlib import ticker
+# import os
+# import numpy as np
+# from scipy import sqrt
+# import subprocess
+# from matplotlib.mlab import griddata
+# from scipy import interpolate
+# import matplotlib
+# matplotlib.use('pdf')
+# import matplotlib.pyplot as plt
+# import matplotlib.gridspec as gridspec
+# from mpl_toolkits.axes_grid1 import make_axes_locatable
+# from matplotlib import ticker
 
 from fortran import NumBAT
 
