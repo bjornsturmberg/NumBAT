b3a6559a73f1fa001ca500dbc3a196718e6436c0
Bjorn Sturmberg <bjorn.sturmberg@mq.edu.au>
2017-09-08 01:47:56 +1000

    More paths

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,6 +20,8 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('../'))
+sys.path.insert(0, os.path.abspath('../'))
+sys.path.insert(0, os.path.abspath('../../'))
 
 # import sys
 # import os
@@ -89,6 +91,7 @@ sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath('../../backend/'))
+sys.path.insert(0, os.path.abspath('../../backend/material_data/'))
 #
 
 # -- General configuration ------------------------------------------------
