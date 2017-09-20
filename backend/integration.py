0eee5dd3be458d164572227391a35bc4ef998855
Bjorn Sturmberg <bjorn.sturmberg@mq.edu.au>
2017-09-20 10:21:01 +1000

    Moved GPL lisence out of doc string and into reg comment.

--- backend/integration.py
+++ backend/integration.py
@@ -1,22 +1,21 @@
-"""
-    mode_calcs.py is a subroutine of NumBAT that contains methods to
-    calculate the EM and Acoustic modes of a structure.
+# mode_calcs.py is a subroutine of NumBAT that contains methods to
+# calculate the EM and Acoustic modes of a structure.
 
-    Copyright (C) 2017  Bjorn Sturmberg, Kokou Dossou,
+# Copyright (C) 2017  Bjorn Sturmberg, Kokou Dossou,
 
-    NumBAT is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+# NumBAT is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-    GNU General Public License for more details.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-"""
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 
 import time
 import numpy as np
