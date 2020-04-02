7891d9c5056862ca20d5a93e70d06789eb2a4d4a
Bjorn Sturmberg <bj.sturmberg@gmail.com>
2020-04-02 23:35:44 +1100

    A little typo.

--- setup.sh
+++ setup.sh
@@ -21,7 +21,7 @@ function makeBAT() {
   echo '#####################################################################'
   echo '        NumBAT and its dependencies have been installed        '
   echo '               and its test cases calculated.  '
-  echo '        Tests outputs are: . passed; E errors; F failed. '
+  echo '        Test outputs are: . passed; E errors; F failed. '
   echo ''
   echo 'NumBAT is brought to you by Bjorn Sturmberg, Kokou Dossou,'
   echo 'Chris Poulton and Michael Steel, with support from CUDOS'
